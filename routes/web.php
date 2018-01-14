5ecd440d6722545dca51ab856ab5d70195877fae
Software-UM <32464947+Software-UM@users.noreply.github.com>
2018-01-14 13:58:32 -0600

    server

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('phpinfo', function(){
+  return view('phpinfo');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
